3654e92f609830b797961a31fa8e1e8d551fbdfd
shark <thetruephil@googlemail.com>
2017-10-19 01:16:17 +0200

    adding loads of documentation and mode-specific functions I introduced a documentation head and started to document the file. But there is still lot's of work to be done.
    Also, I incorporated the lightning-keymap-mode-modes module providing the mode-specific functions needed to perform the evaluations in R or Python. Both of them work smoothly

--- lightning-keymap-mode.el
+++ lightning-keymap-mode.el
@@ -1,33 +1,90 @@
-;;; thunderbolt-mode.el - A lightning fast keymap provided as an Emacs minor mode.
+;;; lightning-keymap-mode.el - A lightning-fast keymap for Emacs.
 
-(defcustom lightning-keymap-toggle-key "<F5>"
-  "Sets a custom key to activate and deactivate `lightning-keymap-mode'.
-Alternatively you can specify the key yourself using `global-set-key' 
-by binding the `lightning-keymap-toggle' function."
-  :group 'lightning-keymap
+;; Author: Philipp Müller <thetruephil@googlemail.com>
+;; Maintainer: Philipp Müller <thetruephil@googlemail.com>
+;; Version: 0.1.0
+;; X-URL: https://github.com/theGreatWhiteShark/lightning-keymap-mode
+;; URL: https://github.com/theGreatWhiteShark/lightning-keymap-mode
+;; Keywords: keymap, navigation
+;; Package-Requires: ((iedit 0.97)(rect-mark 1.4))
+;; Compatibility: GNU Emacs: >21.x
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; In order to use the lightning-keymap-mode you have to add the
+;; following lines to your `.emacs' file.
+;;
+;;    (add-to-list 'load-path "~/PATH-TO-LIGHTNING-KEYMAP-MODE")
+;;    (require 'lightning-keymap-mode)
+;;    (lightning-keymap-mode 1)
+;;
+;;
+;;  Most important variables;
+;;
+;;    `lightning-keymap-toggle-key'
+;;
+;;      A key-sequence specifying a global binding toggling the lightning
+;;      keymap. To prevent this minor mode from setting the global
+;;      key binding, you can initialize the variable with nil.
+;;      Default: "<F5>".
+;;
+;;
+;;  For more information check out the projects Github page:
+;;  https://github.com/theGreatWhiteShark/lightning-keymap-mode
+
+(require 'lightning-keymap-mode-modes)
+
+;; For a more convenient ways of moving between buffers.
+(require 'windmove)
+;; Interactive edit to manipulate multiple identical words at the time
+(require 'iedit)
+;; Rectangle marking for manipulating multiple lines simultaneously.
+(require 'rect-mark)
+
+;;; Customized variables
+(defgroup lightning nil
+	"A lightning-fast keymap for Emacs"
+	:prefix "lightning-"
+	:group 'editing)
+
+(defcustom lightning-keymap-toggle-key "<f5>"
+  "Sets a global key binding to toggle the `lightning-keymap-mode'.
+The input has to be a string and a  valid argument for the `kbd' 
+function.
+
+To prevent this behavior you can set the variable's value to nil."
+  :group 'lightning
   :type 'string)
 
 (defcustom lightning-keymap-basic nil
-  "If set to non-nil all bindings will be activated. Setting it to `t' 
+  "If set to non-nil, all bindings will be activated. Setting it to `t' 
 instead causes the `lightning-keymap-mode' to only bind the keys used
 for navigation (j, k, l, ;) and for newlines (m)."
-  :group 'lightning-keymap
-  :type 'boolean)
+  :group 'lightning
+	:type 'boolean)
 
 ;; customs for the evaluation functions in the three different layers
 ;; custom for activate -bonus: parts, which depend on additional packages
 ;; custom for jumping length
 
-;; easier switchting between buffers (with alt key)
-(require 'windmove)
-(windmove-default-keybindings 'meta)
+;; Defining a global key to toggle the lightning-keymap (unless the
+;; lightning-keymap-toggle-key wasn't set to nil).
+(global-set-key (kbd lightning-keymap-toggle-key)
+'lightning-keymap-mode)
 
-;; enable rectangle marking
-(require 'rect-mark)
-
-;; Interactive edit to manipulate multiple identical words at the time
-(add-to-list 'load-path "~/git/configurations-and-scripts/emacs/iedit")
-(require 'iedit)
 
 ;; Customized keybindings for navigation, killing and various other
 ;; useful things.
@@ -243,9 +300,85 @@ for navigation (j, k, l, ;) and for newlines (m)."
     map)
   "lightning-keymap-mode keymap.")
 
+;;; Mode-specific key bindings
+;; This section contains bindings for two distinct groups of mode.
+;; 1. Modes, which come with an inferior mode providing the user with
+;;    an interactive shell to manipulate their scripts etc. with.
+;;    In order to make the most out of those modes, the
+;;    `lightning-keymap-mode' provides mode-specific functions for
+;;    passing code from the major mode's buffer into its inferior
+;;    counterpart.
+;; 2. There are some minor modes interfering with the keymap provided by
+;;    this one. For now only the `yasnippet' mode came to mi attention.
+;;    If present, the keybindings imposed by those packages have to be
+;;    shadowed in order to ensure `lightning-keymap' will work properly.
+
+;; 1. Mode-specific evaluation functions
+(unless lightning-keymap-basic
+	;; ESS (Emacs speaks statistics) - a very convenient mode for
+	;; manipulating R files.
+	(when (assq 'ess package-alist)
+		(add-hook
+		 'ess-mode-hook
+		 (lambda()
+			 (local-set-key (kbd "M-j") 'left-word)
+			 (local-set-key (kbd "C-n")
+											'lightning-keymap-ess-evaluation-layer-1)
+			 (local-set-key (kbd "<M-n>")
+											'lightning-keymap-ess-evaluation-layer-2)
+			 (local-set-key (kbd "C-M-n")
+											'lightning-keymap-ess-evaluation-layer-3))))
+	;; When manipulating RMarkdown files using `polymode' the export
+	;; function bound to `lightning-keymap-ess-evaluation-layer-3' should
+	;; be working in the markdown part of the document as well.
+	(when (assq 'polymode-minor-mode minor-mode-alist)
+		(add-hook
+		 'markdown-mode-hook
+		 (lambda()
+			 (local-set-key (kbd "C-M-n")
+											'lightning-keymap-ess-evaluation-layer-3))))
+
+	;; Python-mode
+	;; Since I don't seem to find the Python package in the `package-alist'
+	;; variable, I will just check whether there is a function called
+	;; 'python-mode'.
+	(when (functionp 'python-mode)
+		(add-hook
+		 'python-mode-hook
+		 (lambda()
+			 (local-set-key (kbd "M-j") 'left-word)
+			 (local-set-key (kbd "C-n")
+											'lightning-keymap-python-evaluation-layer-1)
+			 (local-set-key (kbd "<M-n>")
+											'lightning-keymap-python-evaluation-layer-2)
+			 (local-set-key (kbd "C-M-n")
+											'lightning-keymap-python-evaluation-layer-3))))
+	;; End of 1. Mode-specific evaluation functions
+	)
+
+;; 2. Shadowing other keybindings
+
+				 
+
 ;; Activating the customized keybindings with every major mode.
 (define-minor-mode lightning-keymap-mode
-  "Enforcing my customized keys on all major modes/"
-  :init-value t
-  :lighter " lightning-keymap")
-(lightning-keymap-mode 1)
+  "Toggle YASnippet mode.
+
+When YASnippet mode is enabled, `yas-expand', normally bound to
+the TAB key, expands snippets of code depending on the major
+mode.
+
+With no argument, this command toggles the mode.
+positive prefix argument turns on the mode.
+Negative prefix argument turns off the mode.
+
+Key bindings:
+\\{yas-minor-mode-map}"
+  :lighter " light"
+  :group 'lightning
+  :global t
+  :keymap lightning-keymap-mode-map)
+
+(provide 'lightning-keymap-mode)
+;;
+;;; End of lightning-keymap-mode.el
